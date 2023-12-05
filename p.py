ed672cd361b01a42ae4ece6c4b0fe2fc8093b2b4
Dexter Pai <dr.pyiephonepai@gmail.com>
2023-12-04 18:56:44 -0600

    comments added

--- p.py
+++ p.py
@@ -4,16 +4,19 @@
 # Usage : copy ingredients and steps into recipe.txt file and python will output <li>[entry]</li> as a result
 # Ready to put into your odin html code.
 
-
+# change this if you want more functionality
 with open ('recipe.txt', 'r') as file:
     recipe = file.read().replace('▢', '').split('\n')
 
+    # remove blank indexes
     for sentences in recipe:
         if sentences == '':
             recipe.pop(recipe.index(sentences))
 
+    # <li> format
     for sentences in recipe:
         recipe[recipe.index(sentences)] = '<li>' + sentences + '</li>'
 
-    for sentences in recipe:
-        print(recipe[recipe.index(sentences)])
+    # print output
+    for output in recipe:
+        print(output)
